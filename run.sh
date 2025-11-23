7de1f5bb521101a729a9e984c40e5742cb58c668
yijingkun <65355323+2466206547@users.noreply.github.com>
2025-11-23 22:56:27 +0800

    Update run.sh
    edit readme

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+PORT=7884 && export http_proxy=http://127.0.0.1:$PORT && export https_proxy=http://127.0.0.1:$PORT && export HTTP_PROXY=http://127.0.0.1:$PORT && export HTTPS_RPOXY=http://127.0.0.1:$PORT
 chmod +x ./clash
 nohup ./clash -f config.yaml -d . > clash.log 2>&1 &
